1849965553ae00cd06477e7465de7a6ea71fd86b
Amarish Kumar Srivastava <92314569+Amarish-2309@users.noreply.github.com>
2023-06-04 22:42:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const num1 =Math.ceil(Math.random()*10);
 const num2 =Math.ceil(Math.random()*10);
 
+
 const questionE1 = document.getElementById("question");
 
 const formE1=document.getElementById("form");
